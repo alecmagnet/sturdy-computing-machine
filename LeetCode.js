38e100bc8bc4d7a9cee32c5a4d4e3573aafa7dfc
Alec Magnet <amagnet@gmail.com>
2021-11-18 12:34:18 -0500

    finalValueAfterOperations

--- LeetCode.js
+++ LeetCode.js
@@ -35,3 +35,14 @@ var subtractProductAndSum = function(n) {
 var defangIPaddr = function(address) {
     return address.replaceAll(".", "[.]")
 };
+
+//https://leetcode.com/problems/final-value-of-variable-after-performing-operations/submissions/
+
+var finalValueAfterOperations = function(operations) {
+    let val = 0
+    for (i = 0; i < operations.length; i++){
+        if (operations[i].includes("+")) val = val + 1
+        if (operations[i].includes("-")) val = val - 1
+    }
+    return val
+};
