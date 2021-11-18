f36c3f11e2ce21d0ae39b194fc05cbf4c45734bd
Alec Magnet <amagnet@gmail.com>
2021-11-18 12:30:54 -0500

    shuffle

--- LeetCode.js
+++ LeetCode.js
@@ -4,3 +4,13 @@ var truncateSentence = function(s, k) {
     return s.split(" ").slice(0, k).join(" ")
 };
 
+// https://leetcode.com/problems/shuffle-the-array/submissions/
+
+var shuffle = function(nums, n) {
+    let retArr = []
+    for (i=0; i<n; i++) {
+        retArr.push(nums[i])
+        retArr.push(nums[n+i])
+    }
+    return retArr
+};
