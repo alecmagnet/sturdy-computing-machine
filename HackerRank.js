63a49c3e15a5b2249f4b2d77f58257ce4d0f26ab
Alec Magnet <amagnet@gmail.com>
2021-11-23 12:05:02 -0500

    sparse arrays solution 1

--- HackerRank.js
+++ HackerRank.js
@@ -1,5 +1,17 @@
 
 
+
+
+
+// https://www.hackerrank.com/challenges/one-month-preparation-kit-sparse-arrays/problem
+
+function matchingStrings(strings, queries) {
+    // solution one
+    return queries.map(e => strings.filter(el => el === e).length)
+
+}	
+
+
 // https://www.hackerrank.com/challenges/one-month-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
 
 function miniMaxSum(arr) {
