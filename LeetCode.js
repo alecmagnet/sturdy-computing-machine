5cfe9ba2d4774447a1135caf7790837b0b8d895e
Alec Magnet <amagnet@gmail.com>
2021-11-22 19:47:39 -0500

    Maximum Nesting Depth of the Parentheses

--- LeetCode.js
+++ LeetCode.js
@@ -1,8 +1,18 @@
 
 
 
+// https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
 
-
+var maxDepth = function(s) {
+    let r = 0
+    let a = s.split('')
+    for (i=0; i<a.length; i++) {
+        let b = a.slice(0, i)
+        let q = b.filter(e => e === '(').length - b.filter(e => e === ')').length
+        if (q > r) r = q
+    }
+    return r
+};
 
 
 // https://leetcode.com/problems/count-good-triplets/submissions/
