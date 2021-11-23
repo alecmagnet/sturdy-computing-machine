99ba843297c68af57d95fef58b9c687a4212ceae
Alec Magnet <amagnet@gmail.com>
2021-11-23 17:56:35 -0500

    persistent bugger

--- CodeWars.js
+++ CodeWars.js
@@ -1,4 +1,18 @@
 
+// https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/javascript
+
+function persistence(num) {
+  let ret = 0
+  if (num < 10) return ret
+  function recursive(n) {
+    ++ret
+    let prod = n.toString().split('').reduce((a,b) => Number(a)*Number(b))
+    if (prod < 10) return ret
+    recursive(prod)
+  }
+  recursive(num)
+  return ret
+}
 
 
 // https://www.codewars.com/kata/5287e858c6b5a9678200083c
