bac156dc8b747a39ebad51b3abb53c19600c44a7
Alec Magnet <amagnet@gmail.com>
2021-11-23 11:23:49 -0500

    min max

--- HackerRank.js
+++ HackerRank.js
@@ -1,5 +1,37 @@
 
 
+// https://www.hackerrank.com/challenges/one-month-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+
+function miniMaxSum(arr) {
+    // Write your code here
+    // const sumArr = arr.map(e => arr.filter(el => el !== e).reduce((a,b) => a+b))
+    // console.log(`${Math.min(...sumArr)} ${Math.max(...sumArr)}`)
+
+    // Attempt two
+    // let minVal = 0
+    // let maxVal = 0
+    // for (i=0; i<arr.length; i++) {
+    //     let sumArr = 
+    // }
+    
+    // Eli's with no for loop
+    // function miniMaxSum(arr) {
+    //     let sum = arr.reduce( (a,b) => a + b )
+    //     let min = Math.min.apply(null, arr);
+    //     let max = Math.max.apply(null, arr);
+    //     let minSum = sum - max;
+    //     let maxSum = sum - min;
+    //     console.log(`${minSum} ${maxSum}`)
+    // }
+
+    // Attempt three
+    let arrTotal = arr.reduce((a,b) => a+b)
+    let sumArr = []
+    for (let i=0; i<arr.length; i++) {
+        sumArr.push(arrTotal - arr[i])
+    }
+    console.log(`${Math.min.apply(null, sumArr)} ${Math.max.apply(null, sumArr)}`)
+}
 
 // https://www.hackerrank.com/challenges/one-month-preparation-kit-plus-minus/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
 
