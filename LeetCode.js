164c2d71a55eb028840651dcfbe14c224cfbb2ff
Alec Magnet <amagnet@gmail.com>
2021-11-22 19:42:51 -0500

    Does my number look big in this

--- LeetCode.js
+++ LeetCode.js
@@ -1,5 +1,21 @@
 
 
+
+
+
+// https://www.codewars.com/kata/5287e858c6b5a9678200083c
+
+function narcissistic(value) {
+  str = value.toString()
+  l = str.length
+  r = 0
+  for (i=0; i<l; i++) {
+    r = r + parseInt(str.charAt(i))**l
+  }
+  return r === value ? true : false
+}
+
+
 // https://leetcode.com/problems/count-good-triplets/submissions/
 
 var countGoodTriplets = function(arr, a, b, c) {
