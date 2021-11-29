a7aa730ac5aa1dde0525450b52d52f3d41c4345c
Alec Magnet <amagnet@gmail.com>
2021-11-29 18:35:48 -0500

    Find The Parity Outlier

--- CodeWars.js
+++ CodeWars.js
@@ -1,5 +1,41 @@
 
 
+
+
+// https://www.codewars.com/kata/5526fc09a1bbd946250002dc
+
+function findOutlier(integers){
+  console.log(integers)
+  let n = integers.length
+  let ret = null
+  for (i=0; i<n; i++) {
+    let r = integers[i]
+    if (
+      (
+        r%2 === 0 
+      &&
+        integers[((i+1) % n + n)%n]%2 !==0
+      && 
+        integers[((i+2) % n + n) % n]%2 !== 0
+      )
+    ||
+      (
+        r%2 !== 0 
+      &&
+        integers[((i+1) % n + n)%n]%2 ===0
+      && 
+        integers[((i+2) % n + n) % n]%2 === 0
+      )
+    ) {  
+      ret = r
+      break
+    } else {
+      continue
+    }
+  }
+  return ret
+}
+
 // https://www.codewars.com/kata/55b4d87a3766d9873a0000d4/train/javascript
 
 function howmuch(m, n) {
