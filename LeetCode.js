a02569c83db43949fbcad2060fa90d00ec953ddb
Alec Magnet <amagnet@gmail.com>
2021-11-30 15:02:12 -0500

    Squares of a Sorted Array

--- LeetCode.js
+++ LeetCode.js
@@ -1,4 +1,21 @@
 
+// IMPORTANT https://leetcode.com/problems/squares-of-a-sorted-array
+
+var sortedSquares = function(nums) {
+    let r = []
+    let start = 0
+    let end = nums.length - 1
+    let pos = end
+    while (start <= end) {
+        if (nums[start]**2 > nums[end]**2) {
+            r[pos--] = nums[start++]**2
+        } else {
+            r[pos--] = nums[end--]**2
+        }
+    }
+    return r
+};
+
 
 
 // https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
