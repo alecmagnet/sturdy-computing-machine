ee14d8fecfd19f3bb887b95d8deb72a0d659d1c1
Alec Magnet <amagnet@gmail.com>
2021-11-30 12:40:20 -0500

    flipping bits

--- HackerRank.js
+++ HackerRank.js
@@ -1,5 +1,23 @@
 
 
+// https://www.hackerrank.com/challenges/one-month-preparation-kit-flipping-bits/problem
+
+function flippingBits(n) {
+    let oneStr = (n).toString(2)
+    let str = ""
+    if (oneStr.length < 32) {
+        for (let j=0; j<(32-oneStr.length); j++){
+            str = "0" + str
+        }
+        oneStr = str + oneStr
+    }
+    let ret = ""
+    for (let i=0; i<32; i++) {
+        oneStr.charAt(i) == "0" ? ret = ret + "1" : ret = ret + "0"
+    }
+    return parseInt(ret, 2)
+}
+
 // https://www.hackerrank.com/challenges/one-month-preparation-kit-time-conversion/problem
 
 function timeConversion(s) {
