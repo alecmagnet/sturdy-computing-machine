2af22fe79d5aaa85a5de1e727f8ce7fd3ca1a21c
Alec Magnet <amagnet@gmail.com>
2021-11-30 15:41:22 -0500

    Human Readable Time

--- CodeWars.js
+++ CodeWars.js
@@ -1,4 +1,13 @@
 
+// https://www.codewars.com/kata/52685f7382004e774f0001f7/
+
+function humanReadable (seconds) {
+  const pad = (n) => n < 10 ? `0${n}` : `${n}`
+  let hrs = Math.floor(seconds/3600)
+  let mins = Math.floor((seconds % 3600)/60)
+  let secs = seconds - (mins*60) - (hrs*3600)
+  return pad(hrs)+":"+pad(mins)+":"+pad(secs);
+}
 
 
 // https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
